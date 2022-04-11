4e62d2d5e7fcbdfe1d97512f4f2a37ef621481ae
Luis Forero <lforero@hubb.me>
2022-04-11 18:46:05 -0500

    Add initial entities

---
+++ Code/2022-02-11/2022-02-11.Entities/Models/ApiErrorResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_02_11.Entities.Models
+{
+    public class ApiErrorResponse
+    {
+        public string Message { get; set; }
+        public string[] Errors { get; set; }
+        public bool IsSuccess { get; set; }
+
+        //public Userinfo UserInfo { get; set; }
+        //public DateTime ExpireDate { get; set; }
+    }
+}
