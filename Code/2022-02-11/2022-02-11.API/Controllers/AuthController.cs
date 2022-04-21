81813ce08d41930f93ea30944e0c991939530dd2
Luis Forero <lforero@hubb.me>
2022-04-20 21:36:12 -0500

    Add Register Action

--- Code/2022-02-11/2022-02-11.API/Controllers/AuthController.cs
+++ Code/2022-02-11/2022-02-11.API/Controllers/AuthController.cs
@@ -1,7 +1,5 @@
 ﻿using _2022_02_11.Entities.Models;
 using _2022_02_11.Services;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _2022_02_11.API.Controllers
@@ -23,6 +21,18 @@ namespace _2022_02_11.API.Controllers
         public async Task<IActionResult> Login(LoginRequest model)
         {
             var result = await _usersService.GenerateTokenAsync(model);
+            if (result.IsSuccess)
+                return Ok(result);
+
+            return BadRequest(result);
+        }
+
+        [ProducesResponseType(200, Type = typeof(OperationResponse<string>))]
+        [ProducesResponseType(400, Type = typeof(OperationResponse<string>))]
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(RegisterRequest model)
+        {
+            var result = await _usersService.RegisterUserAsync(model);
             if (result.IsSuccess)
                 return Ok(result);
 
---
+++ Code/2022-02-11/2022-02-11.Entities/Models/OperationResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace _2022_02_11.Entities.Models
+{
+    public class OperationResponse<T> : BaseResponse
+    {
+        public T Data { get; set; }
+    }
+}
