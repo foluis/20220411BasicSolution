409cf76ac47dd91feaeadf8fbb0997e518d2df2d
Luis Forero <lforero@hubb.me>
2022-04-25 23:19:18 -0500

    Basic operations UserProfile

---
+++ Code/2022-02-11/2022-02-11.Entities/Models/CollectionResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace _2022_02_11.Entities.Models
+{
+    public class CollectionResponse<T> : BaseResponse
+    {
+        public int? PageNumber { get; set; }
+        public int? PagesCount { get; set; }
+        public int? PageSize { get; set; }
+        public IEnumerable<T> Records { get; set; }
+    }
+}
